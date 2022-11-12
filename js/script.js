821ba21cd151e7f30c990f21223fbf47e8ca6470
Liakos <liakos.prekas@gmail.com>
2022-11-12 22:47:48 +0200

    JS Optimize

--- js/script.js
+++ js/script.js
@@ -2,18 +2,24 @@ fetch('https://restcountries.com/v3.1/all')
   .then(res => res.json())
   .then(data =>
     data.forEach(x => {
-      let html = `<div class="card">
-      <div class="flag">
-        <img src="${x.flags.svg}" alt="flag">
-      </div>
-      <div class="country-details">
-        <h4 class="country"> ${x.name.common} </h4>
-        <h5 class="population"> Population: <span class="population-span">${x.population}</span></h5>
-        <h5 class="region"> Region: <span class="region-span">${x.region}</span></h5>
-        <h5 class="capital"> Capital: <span class="capital-span">${x.capital}</span></h5>
-      </div>
-    </div>`
+      let html = createCard(x.flags.svg, x.name.common, x.population, x.region, x.capital)
+
       document.getElementsByClassName('cards-container')[0].innerHTML += html
     })
   )
   .catch(error => console.log(error))
+
+
+const createCard = (img, name, population, region, capital) => {
+  return `<div class="card">
+            <div class="flag">
+              <img src="${img}" alt="flag">
+            </div>
+            <div class="country-details">
+              <h4 class="country"> ${name} </h4>
+              <h5 class="population"> Population: <span class="population-span">${population}</span></h5>
+              <h5 class="region"> Region: <span class="region-span">${region}</span></h5>
+              <h5 class="capital"> Capital: <span class="capital-span">${capital}</span></h5>
+            </div>
+          </div>`
+}
